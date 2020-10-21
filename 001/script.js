a5d1a18dd2fe00c9d618ad649f81d9715bca353d
Kerilet <jivore78@gmail.com>
2020-10-21 20:30:15 -0300

    adicionei um interruptor;

---
+++ 001/script.js
@@ -0,0 +1,9 @@
+var mensagem = 'Eu andei dando uma pesquisada em JavaScript';
+var num = 1;
+var num2 = "1";
+
+alert(mensagem);
+
+mensagem = "Já que o Jessé n passou nd pra mim fazer";
+
+alert(mensagem + " de lição de casa");
