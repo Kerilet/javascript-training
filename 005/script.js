10dc5818a58ca1e77cacc225e39372862f81a5a8
Kerilet <jivore78@gmail.com>
2020-11-04 21:01:27 -0300

    pfvr salva

--- 005/script.js
+++ 005/script.js
@@ -1,51 +1,36 @@
-Quais são seus esportes preferidos?
+const firstQuestion = parseFloat(prompt(`Quais são seus esportes preferidos?
 
 1 - Futebol, automobilismo, esportes radicais
 2 - Squash , boliche, voleibol
 3 - Aeróbica, spinning
 4 - Patinação no Gelo, Ginástica Olímpica
-5 - Caça a ursos
+5 - Caça a ursos`));
 
-
-Que comidas você prefere?
+const secondQuestion = parseFloat(prompt(`Que comidas você prefere?
 
 1 - Churrasco, Massas, Frituras 
 2 - Peixe e salada
 3 - Sanduíches integrais
 4 - Aves acompanhadas de vegetais cozidos no vapor
-5 - Capivara, javali, comida muito apimentada
+5 - Capivara, javali, comida muito apimentada`));
 
-
-Na hora do banho, você costuma...
+const thirdQuestion = parseFloat(prompt(`Na hora do banho, você costuma...
 
 1 - Toma banho rápido, usa xampu e esquece das orelhas ou do pescoço
 2 - Toma banho sem pressa mas não exagera
 3 - Demora mais de meia hora e usa sabonete líquido
 4 - Toma banho com sais e espuma na banheira
-5 - Toma banho em 1 minuto, usa sabão em barra
+5 - Toma banho em 1 minuto, usa sabão em barra`));
 
-
-Qual sua relação com o espelho?
+const fourthQuestion = parseFloat(prompt(`Qual sua relação com o espelho?
 
 1 - Usa para fazer barba
 2 - Admira sua pele e observa seus músculos
 3 - Idem c, e ainda analisa a bunda
 4 - Admira-se com diferentes camisas e penteados
-5 - Não usa
+5 - Não usa`));
 
 
 
-
-Pontos:
-1 - 20
-2 - 10
-3 - 0
-4 - -10
-5 - 30
-
-Média:
-< 0 - Bichona
-0 a 10 - Hmmmmmm
-10 a 20 - Homem
-20 a 30 - Macho
->= 30 - Rambo!
+const finalAnwser = firstQuestion + secondQuestion + thirdQuestion + fourthQuestion / 4;
+const anwserReveal = confirm(`Sua média de pontos foi ${finalAnwser}, portanto você é ${results}`)
