ef47e28d2e719d6b76baaa79c355bff9349a2fc8
Kerilet <jivore78@gmail.com>
2020-11-09 12:11:46 -0300

    média adicionada e calculada;

--- 005/script.js
+++ 005/script.js
@@ -30,7 +30,102 @@ const fourthQuestion = parseFloat(prompt(`Qual sua relação com o espelho?
 4 - Admira-se com diferentes camisas e penteados
 5 - Não usa`));
 
+const firstAnwser = 10;
+const secondAnwser = 10;
+const thirdAnwser = 10;
+const fourthAnwser = 10;
 
+if (firstQuestion === 1) {
+    firstAnwser === 20
+} else {
+    if (firstQuestion === 2) {
+        firstAnwser === 10
+    } else {
+        if (firstQuestion === 3) {
+            firstAnwser === 0
+        } else {
+            if (firstQuestion === 4) {
+                firstAnwser === -10
+            } else {
+                firstAnwser === 30
+            }
+        }
+    }
+}
 
-const finalAnwser = firstQuestion + secondQuestion + thirdQuestion + fourthQuestion / 4;
-const anwserReveal = confirm(`Sua média de pontos foi ${finalAnwser}, portanto você é ${results}`)
+if (secondQuestion === 1) {
+    secondAnwser === 20
+} else {
+    if (secondQuestion === 2) {
+        secondAnwser === 10
+    } else {
+        if (secondQuestion === 3) {
+            secondAnwser === 0
+        } else {
+            if (secondQuestion === 4) {
+                secondAnwser === -10
+            } else {
+                secondAnwser === 30
+            }
+        }
+    }
+}
+
+if (thirdQuestion === 1) {
+    thirdAnwser === 20
+} else {
+    if (thirdQuestion === 2) {
+        thirdAnwser === 10
+    } else {
+        if (thirdQuestion === 3) {
+            thirdAnwser === 0
+        } else {
+            if (thirdQuestion === 4) {
+                thirdAnwser === -10
+            } else {
+                thirdAnwser === 30
+            }
+        }
+    }
+}
+
+if (fourthQuestion === 1) {
+    fourthAnwser === 20
+} else {
+    if (fourthQuestion === 2) {
+        fourthAnwser === 10
+    } else {
+        if (fourthQuestion === 3) {
+            fourthAnwser === 0
+        } else {
+            if (fourthQuestion === 4) {
+                fourthAnwser === -10
+            } else {
+                fourthAnwser === 30
+            }
+        }
+    }
+}
+
+var results = 10;
+const finalAnwser = firstAnwser + secondAnwser + thirdAnwser + fourthAnwser / 4;
+
+if (finalAnwser < 0) {
+    results === Bichona
+} else {
+    if (finalAnwser > 10) {
+        results === Hmmmmmm
+    } else {
+        if (finalAnwser > 20) {
+            results === Homem
+        } else {
+            if (finalAnwser > 30) {
+                results === Macho
+            } else {
+                results === Rambo
+            }
+        }
+    }
+}
+
+const anwserReveal = confirm(`Sua média de pontos foi ${finalAnwser}, portanto você é ${results}`);
