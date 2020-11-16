549c98258324f6cd77301ecf42fa65929651c370
Kerilet <jivore78@gmail.com>
2020-11-16 10:55:09 -0300

    commit diario

--- 006/script.js
+++ 006/script.js
@@ -1,11 +1,18 @@
 const number = parseInt(prompt('Please, enter a prime number'));
-let isPrime = true;
+let isPrime = false;
 let index = 2;
 
-while (isPrime) {
-    // do something here
-    index = index + 1;
-}
+if (number === 2 || number === 3) {
+    isPrime = true
+    alert(`Thank you for not being a complete dumbass!`)
+} else {
+    while (isPrime === false) {
+        parseInt(prompt('Please, enter a prime number'));
+        index = index + 1;
+    }
+};
+
+
 
 // and here you die
 // thank you my master :)
