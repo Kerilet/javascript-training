f190c07c22b9883f73244f179e1e78b9c296787a
Kerilet <jivore78@gmail.com>
2020-11-16 21:13:58 -0300

    someone call an ambulance "for" me

--- 007/script.js
+++ 007/script.js
@@ -1,33 +1,16 @@
-function percentage(partialValue, totalValue) {
-    return (100 * partialValue) / totalValue;
-}
 const price = parseInt(prompt('What\'s the price?'));
 const payments = parseInt(prompt('How many payments?'));
-let rate = percentage(2, 100);
-let previousPaymentPrice = price / payments;
-let currentPaymentPrice = previousPaymentPrice + rate;
-let futurePaymentsPrice = currentPaymentPrice + rate;
+const rate = parseFloat(prompt('What\'s the interest rate(%) (monthly)?'));
+const paymentPrice = price / payments;
+
+let amount = 0;
+let lastPaymentPrice = paymentPrice;
 
 
+for (let i = 0; i < payments; i++) {
+    lastPaymentPrice = lastPaymentPrice + (lastPaymentPrice * (rate / 100));
+    amount = amount + lastPaymentPrice;
+}
 
 
-/* Informações importantes:
-
- 1 - valor da parcela atual
- 
- 2 - valor da parcela anterior
- 
- 3 - valor da próxima parcela
-
- */
-
-
-
-/* Procedimentos necessários 
-
-1 - laço com o numero das parcelas aplicando o juros sob o valor anterior
-
-2 - 
-*/
-
-// alert('You will pay ${}');
+document.write(`You wil pay ${amount.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`);
