01e7710c5237aebbfa10bfb4cdec11a8babebeac
Kerilet <jivore78@gmail.com>
2020-11-25 23:52:08 -0300

    2/3 tasks done!

--- 009/script.js
+++ 009/script.js
@@ -1,8 +1,19 @@
-function hours() {
-    const hours = parseFloat(prompt(`How many hours to you want to convert into seconds?`))
-    const conversion = hoursToSeconds(hours)
+function primeQuestion() {
+    const num = parseInt(prompt('Please, enter a prime number'));
+    let isPrime = true;
+    const info = primeCheck(num, isPrime)
 }
 
-function hoursToSeconds(hours) {
-    document.body.innerHTML += `${hours} hour(s) will be converted into ${hours * 3600} seconds! Have a nice day/evening/night!<br>`;
+function primeCheck(num, isPrime) {
+    for (let index = num; index >= 1; index--) {
+        if (index % 2 === 0 && index > 2 || index % 3 === 0 && index > 3 || index % 5 === 0 && index > 5) {
+            for (let i = 2; i < num; i++) {
+                if (num % i === 0) isPrime = false;
+            }
+            document.body.innerHTML += `${index} is not prime!<br>`;
+
+        } else {
+            document.body.innerHTML += `${index.toString().padStart(3,'0')} is prime!<br>`;
+        }
+    }
 }
