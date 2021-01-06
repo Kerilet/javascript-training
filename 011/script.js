b9de34a0d6d59c636caff6aae00f91df48c3e39f
Kerilet <jivore78@gmail.com>
2021-01-06 17:16:46 -0300

    reset kinda working

--- 011/script.js
+++ 011/script.js
@@ -1,12 +1,24 @@
-const matrix = [
+let matrix = [
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
 ];
 
 let turn = 0;
+/*const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
+*/
 
+//const omniCell = document.querySelectorAll('[data-cellElement');
 
 // eslint-disable-next-line no-unused-vars
 function addPosition() {
@@ -16,18 +28,20 @@ function addPosition() {
     const rowLabel = ['A', 'B', 'C'];
     const rowNumber = rowLabel.indexOf(rowLetter.toUpperCase());
     if (rowNumber >= 0 && rowNumber < 3 && columnNumber >= 0 && columnNumber < 3) {
-        const currentTurn = turn % 2 === 0 ? 'O' : 'X';
+        const currentTurn = turn % 2 === 0 ? '<div class = "evenTurn cellElement">O</div>' : '<div class = "oddTurn cellElement">X</div>';
         if (matrix[rowNumber][columnNumber] === '') {
             matrix[rowNumber][columnNumber] = currentTurn;
             writeInfo();
             turn = turn + 1;
+            document.getElementById('position').value = '';
+            //checkWinner(currentTurn, omniCell);
         } else {
             alert('You can not input your movement on a occupied cell you stupid degenerate that should not have existed, stop wasting our oxygen you oxymoron');
         }
     } else {
         alert('Please enter valid information you stupid degenerate that should not have existed, stop wasting our oxygen you oxymoron');
     }
-    checkWinner();
+
     document.getElementById('position').value = '';
 }
 
@@ -42,14 +56,30 @@ function writeInfo() {
         info += '</tr>';
     }
 
-    document.getElementById('jojo').innerHTML = info;
+    document.getElementById('checkers').innerHTML = info;
 }
 
-function checkWinner() {
-
+/* function checkWinner(currentTurn, omniCell) {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return omniCell[index].classList.contains(currentTurn);
+        });
+    });
 }
+*/
 
 writeInfo();
+
+// eslint-disable-next-line no-unused-vars
+function resetBoard() {
+    matrix = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+    ];
+    document.getElementById('boardGame').value = '';
+}
+
 
 /*
 Liçon di KSa : comprar tompero; adicionar o botão reset; no final mostrar o resultado;
