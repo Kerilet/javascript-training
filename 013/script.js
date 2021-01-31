87fcfb65a503e3d5bce7b27331deb79e5f8ae3ad
Kerilet <jivore78@gmail.com>
2021-01-31 00:14:00 -0300

    "kono dio da" added w/ sfx included;

--- 013/script.js
+++ 013/script.js
@@ -1,36 +1,8 @@
-// Tradicional :(
-function myFn1() {
-    console.log('I am myFn1');
-}
-
-// Tradicional 2 :(
-const myFn2 = function fn() {
-    console.log('I am myFn2');
-};
-
-// Moderna!!! :)
-const myFn3 = (number) => {
-    return number * 10;
-};
-
-// Moderna e concisa
-const myFn4 = number => number * 10;
-
-myFn1();
-myFn2();
-console.log(myFn3(123));
-console.log(myFn4(456));
-
-/*
-funcoes de callback
- eventos sem previsao de acontecimento (ex: garcon retornando comida)
- sempre recebe uma funcao como parametro, e sempre executa uma funcao ao "ativar"
-*/
-
 let codes1 = [];
 
-
 let codes2 = [];
+
+let codes3 = [];
 
 const konamiKode = [
     'ArrowUp',
@@ -62,9 +34,26 @@ const barrelRoll = [
     'l',
 ].join('-');
 
+const konoDio = [
+    'k',
+    'o',
+    'n',
+    'o',
+    'd',
+    'i',
+    'o',
+    'd',
+    'a',
+].join('-');
+
 const writeKonamiCode = () => {
     document.body.innerHTML += '<img src="../img/rickroll.gif" alt="Konami Kode duuude!" loading="here\'s an easter egg!">';
     playAudio();
+};
+
+const konoDioDa = () => {
+    document.body.innerHTML += '<img src="../img/konodioda.gif" alt="You thought this was an alt, but it was I, Dio!" loading="here\'s Dio!">';
+    playAudio3();
 };
 
 const playAudio = () => {
@@ -77,6 +66,11 @@ const playAudio2 = () => {
     audio.play();
 };
 
+const playAudio3 = () => {
+    const audio = document.getElementById('audio3');
+    audio.play();
+};
+
 const rotateScreen = () => {
     var a = '-webkit-',
         b = 'transform:rotate(1turn);',
@@ -85,19 +79,10 @@ const rotateScreen = () => {
 };
 
 const spinScreen = () => {
-    var a = '-webkit-', // vendor prefix for Chrome, Safari
-        b = 'transform:rotate(1turn);', // the CSS for rotating 360deg
-        c = 'transition:4s;'; // the CSS for making the rotation last 4 seconds
-
-    document.head.innerHTML // adding a style tag to the <head>
-        += '<style>body{' + a + b + a + c + b + c; // the combined CSS in the style tag
-
-    /*
-    This actually generates a string that looks like:
-    "<style>body{-webkit-transform:rotate(1turn);-webkit-transition:4s;transform:rotate(1turn);transition(4s);"
-    Which obviously is lacking a closing tag and a closing bracket, but luckily browsers are smart enough to figure this out.
-    It also only has vendor prefixes for WebKit. That's because it turns out Firefox and Opera work just fine without the prefixes here.
-    */
+    var a = '-webkit-',
+        b = 'transform:rotate(1turn);',
+        c = 'transition:4s;';
+    document.head.innerHTML += '<style>body{' + a + b + a + c + b + c;
 };
 
 const writeBarrelRoll = () => {
@@ -116,7 +101,7 @@ const writeBarrelRoll = () => {
     playAudio2();
 };
 
-const barrelCheck = (callback /* is a function */ ) => {
+const barrelCheck = (callback) => {
     document.addEventListener('keyup', (event) => {
         codes2.push(event.key);
         const joined = codes2.join('-');
@@ -131,7 +116,7 @@ const barrelCheck = (callback /* is a function */ ) => {
     });
 };
 
-const konamiCheck = (callback /* is a function */ ) => {
+const konamiCheck = (callback) => {
     document.addEventListener('keyup', (event) => {
         codes1.push(event.key);
         const joined = codes1.join('-');
@@ -146,20 +131,21 @@ const konamiCheck = (callback /* is a function */ ) => {
     });
 };
 
+const dioCheck = (callback) => {
+    document.addEventListener('keyup', (event) => {
+        codes3.push(event.key);
+        const joined = codes3.join('-');
+        if (konoDio.startsWith(joined)) {
+            if (konoDio === joined) {
+                callback();
+            }
+        } else {
+            codes3 = [];
+        }
+        console.log(event, codes3);
+    });
+};
+
 konamiCheck(writeKonamiCode);
 barrelCheck(writeBarrelRoll);
-// konamiCheck(showAJojoChar);
-
-// faça o barrel roll
-/*
-        <script>
-            var doABarrelRoll = function() {
-                var a = "-webkit-",
-                    b = 'transform:rotate(1turn);',
-                    c = 'transition:4s;';
-                document.head.innerHTML += '<style>body{' + a + b + a + c + b + c
-            }
-
-            doABarrelRoll();
-        </script>
-*/
+dioCheck(konoDioDa);
