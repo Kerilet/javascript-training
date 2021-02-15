bb1a23ff9e986c03ab39336ba08b51fb9915f232
Kerilet <jivore78@gmail.com>
2021-02-15 11:01:33 -0300

    fml

--- 015/script.js
+++ 015/script.js
@@ -1,26 +1,3 @@
-import { addInfo, deleteInfo, searchInfo, deleteSingle, writeInfo } from "./script3.js";
+import { addEnter } from "./script3.js";
 
-window.addInfo = addInfo;
-window.deleteInfo = deleteInfo;
-window.searchInfo = searchInfo;
-window.writeInfo = writeInfo;
-window.deleteSingle = deleteSingle;
-
-
-
-/*
-lista de objetos
-
-toDo {
-  "completed": false
-  "text":
-}
-
-possible functions :
-insert new toDo - press enter to do so.
-delete an existing toDo - click a button to do so.
-toggle the state of existing toDo - click a button to do so. -- modify object
-clear all completed toDo - click a button to do so.
-filter toDos by their state - click a menu to do so.  -- use object data
-edit a toDo - double click to do so.  -- modify object
-*/
+window.addEnter = addEnter;
--- 015/script3.js
+++ 015/script3.js
@@ -1,56 +1,143 @@
-const infoList = [];
+let infoList = [];
+let status = 'ALL';
 
-const addInfo = () => {
-    const toDos = document.getElementById('toDos').value;
+const addEnter = (ev) => {
+    console.log(ev.target.value)
+        // addInfo(ev.target.value);
+}
+
+const addInfo = (targetValue) => {
+    const toDos = targetValue;
     if (toDos == '') {
         alert('Please enter valid information!');
     } else {
-        infoList.push(`<li class="list-group-item"><div id="allInfo">${toDos}</div></li>`);
+        infoList.push({
+            text: toDos,
+            completed: false,
+        });
         writeInfo();
         document.getElementById('toDos').value = '';
     }
 }
 
-const deleteInfo = () => {
-    if (infoList.length < -1) {
-        alert('There is nothing to delete!');
-    } else {
-        infoList.splice(0, infoList.length);
-        writeInfo();
-    }
+const deleteCompleted = () => {
+    infoList = infoList.filter((el) => !el.completed);
+    writeInfo();
 }
 
-const searchInfo = () => {
-    const input = document.getElementById('search');
-    const filter = input.value.toUpperCase();
-    const ul = document.getElementById('items');
-    const li = ul.getElementsByTagName('li');
-    for (let i = 0; i < li.length; i++) {
-        const divFinder = li[i].getElementsByTagName('div')[0];
-        const txtValue = divFinder.textContent || divFinder.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = '';
-        } else {
-            li[i].style.display = 'none';
-        }
-    }
-}
-
-// eslint-disable-next-line no-unused-vars
-const deleteSingle = () => {
-    const desiredNum = prompt('Which toDo you would you like to delete?');
-    for (let index = 0; index < infoList.length; index++) {
-        if (infoList[index].indexOf(`${desiredNum}`) > -1) {
-            infoList.splice(index, 1);
-        }
-    }
+const filterInfo = (newStatus) => {
+    status = newStatus;
     writeInfo();
 }
 
 
-const writeInfo = () => {
-    infoList.sort();
-    document.getElementById('items').innerHTML = infoList.join('');
+const deleteSingle = (index) => {
+    infoList.splice(index, 1);
+    writeInfo();
 }
 
-export { addInfo, deleteInfo, searchInfo, deleteSingle, writeInfo }
+const updateInfo = (indexu, eventuru) => {
+    infoList[indexu].text = eventuru.target.value;
+}
+
+const toggleState = (indexuru) => {
+    infoList[indexuru].completed = !infoList[indexuru].completed;
+    writeInfo();
+}
+
+const tasksLeft = () => {
+    let left = 0;
+    for (let a = 0; a < infoList.length; a++) {
+        if (!infoList[a].completed) {
+            left++;
+        }
+    }
+    return left === 1 ? `${left} item left` : `${left} items left`;
+};
+
+const writeInfo = () => {
+    const filteredList = [];
+    for (let y = 0; y < infoList.length; y++) {
+        if (status === 'ACTIVE' && infoList[y].completed === false) {
+            filteredList.push(infoList[y]);
+        } else if (status === 'COMPLETED' && infoList[y].completed === true) {
+            filteredList.push(infoList[y]);
+        } else if (status === 'ALL') {
+            filteredList.push(infoList[y]);
+        }
+    }
+
+    document.getElementById('items').innerHTML = '';
+    document.getElementById('footer').innerHTML = '';
+
+    for (let i = 0; i < filteredList.length; i++) {
+        const btn = document.createElement('button');
+        btn.innerHTML = 'X';
+        btn.addEventListener('click', () => deleteSingle(i));
+
+
+        const noU = document.createElement('input');
+        noU.setAttribute('type', 'checkbox');
+        noU.checked = filteredList[i].completed;
+        noU.addEventListener('click', () => toggleState(i));
+
+
+        const input = document.createElement('input');
+        input.value = filteredList[i].text;
+        input.addEventListener('input', (ev) => updateInfo(i, ev));
+
+        const line = document.createElement('div');
+        line.append(noU);
+        line.append(input);
+        line.append(btn);
+
+        document.getElementById('items').append(line);
+
+    }
+
+    if (infoList.length > 0) {
+        const counterDIV = document.createElement('div');
+        counterDIV.innerHTML = tasksLeft();
+
+        const filterDIV = document.createElement('div');
+        const allBTN = document.createElement('button');
+        allBTN.innerHTML = 'All';
+        allBTN.addEventListener('click', () => filterInfo('ALL'));
+
+        const activeBTN = document.createElement('button');
+        activeBTN.innerHTML = 'Active';
+        activeBTN.addEventListener('click', () => filterInfo('ACTIVE'));
+
+        const completedBTN = document.createElement('button');
+        completedBTN.innerHTML = 'Completed';
+        completedBTN.addEventListener('click', () => filterInfo('COMPLETED'));
+
+        filterDIV.append(allBTN);
+        filterDIV.append(activeBTN);
+        filterDIV.append(completedBTN);
+
+        const btn2 = document.createElement('button');
+        btn2.innerHTML = 'Delete all completed tasks';
+        btn2.addEventListener('click', () => deleteCompleted());
+
+        const line2 = document.getElementById('footer');
+        line2.append(counterDIV);
+        line2.append(filterDIV);
+
+        let existsCompleted = false;
+
+        for (let x = 0; x < filteredList.length; x++) {
+            if (filteredList[x].completed === true) {
+                existsCompleted = true;
+            }
+        }
+        if (existsCompleted) {
+            line2.append(btn2)
+        }
+        document.getElementById('items').after(line2);
+    }
+
+    // document.getElementById('items').append(JSON.stringify(filteredList));
+
+}
+export { addEnter }
